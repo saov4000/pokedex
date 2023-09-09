ed7c51a5bf155efb207311e806e3bfbb3009d95c
saov4000 <saov4000@github.com>
2023-09-09 07:08:03 -0300

    third commit

--- assets/js/pokeapi.js
+++ assets/js/pokeapi.js
@@ -13,21 +13,8 @@ function convertPokeAPIdetailToPokemon(pokeDetail){
     return pokemon
 }
 
-function convertPokeAPIdetailToPokemon2(pokeDetail){
-    const pokemon = new Pokemon()
-    pokemon.id = pokeDetail.id
-    pokemon.name = pokeDetail.name
-    const types = pokeDetail.types.map((typeSlot)=> typeSlot.type.name)
-    const [type] = types
-    pokemon.types = types 
-    pokemon.type = type
-    pokemon.image = pokeDetail.sprites.other.dream_world.front_default
-    return pokemon
-}
-
 function convertToDetails(pokeDetail){
     const pokemon = new Pokemon()
-
     pokemon.hp = pokeDetail.hp
     pokemon.atack = pokeDetail.atack
     pokemon.defense = pokeDetail.defense
@@ -42,18 +29,6 @@ pokeAPI.getPokemonDetails = (pokemon)=>{
     .then((response)=>response.json())
     .then(convertPokeAPIdetailToPokemon)
 }
-
-/*pokeAPI.getPokemonDetails2 = (pokemon)=>{
-    return fetch(pokemon)
-    .then((response)=>response.json())
-    .then(convertPokeAPIdetailToPokemon2)
-}*/
-
-/*pokeAPI.getPokemonDetailsPage = (pokemon)=>{
-    return fetch(pokemon.url)
-    .then((response)=>response.json())
-    .then(convertToDetails)
-}*/
 
 pokeAPI.getOnePokemon = (id) =>{
     const url = `https://pokeapi.co/api/v2/pokemon/${id}/`
